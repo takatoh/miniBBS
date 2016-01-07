074fa37827bd09639c48708f4d770061bcf4e5ae
takatoh <takatoh.m@gmail.com>
2016-01-07 22:36:52 +0900

    bbs.php: New.

---
+++ bbs.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ *  Utilities
+ */
+
+  function files_sort_by_mtime($files) {
+    usort($files, "order_by_mtime");
+    return $files;
+  }
+
+  function order_by_mtime($a, $b) {
+    $mtime_a = filemtime($a);
+    $mtime_b = filemtime($b);
+    if ($mtime_a > $mtime_b) {
+      return -1;
+    } else if ($mtime_a < $mtime_b) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+
+/*
+ *  Classes
+ */
+
+  // Message class
+  class Message {
+    public $id;
+    public $name;
+    public $subject;
+    public $content = "";
+
+    function __construct($file) {
+      $contents = file_get_contents($file);
+      $items = explode("---- comment\n", $contents);
+      $mes = rtrim(array_shift($items));
+      $lines = explode("\n", $mes);
+      array_shift($lines);
+      $id = array_shift($lines);
+      $id = mb_ereg_replace("id: ", "", $id);
+      $this->id = $id;
+      $name = array_shift($lines);
+      $name = mb_ereg_replace("name: ", "", $name);
+      $this->name = $name;
+      $subject = array_shift($lines);
+      $subject = mb_ereg_replace("subject: ", "", $subject);
+      $this->subject = $subject;
+      foreach ($lines as $line) {
+        $this->content = $this->content . $line . "<br>";
+      }
+    }
+  }
+
+  function message_new($file) {
+    return new Message($file);
+  }
+?>
