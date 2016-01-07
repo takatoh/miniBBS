caf0ee026ba04938530a8665a88438a4f0d56fd3
takatoh <takatoh.m@gmail.com>
2016-01-08 05:00:38 +0900

    index.php: Now, posting message is available.

--- index.php
+++ index.php
@@ -11,12 +11,36 @@
     }
   }
 
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    rsort($files);
+    $matches = array();
+    preg_match("!data/(.+)\.mes!", $files[0], $matches);
+    $last_message_id = $matches[1];
+    $new_message_id = $last_message_id + 1;
+    $new_message_file = "data/" . sprintf("%04d", $new_message_id) . ".mes";
+
+    $fp = fopen($new_message_file, "wb");
+    if ($fp) {
+      if (flock($fp, LOCK_EX)) {
+        fwrite($fp, "==== message\n");
+        fwrite($fp, "id: " . $new_message_id . "\n");
+        fwrite($fp, "name: " . $_POST['name'] . "\n");
+        fwrite($fp, "subject: " . $_POST['subject'] . "\n");
+        fwrite($fp, $_POST['content'] . "\n");
+        flock($fp, LOCK_UN);
+      }
+      fclose($fp);
+      array_push($files, $new_message_file);
+    }
+  }
+
   $files = files_sort_by_mtime($files);
   $messages = array_map("message_new", $files);
 ?>
 <!DOCTYPE HTML>
 <html>
   <head>
+    <meta charset="UTF-8" />
     <title>mini BBS</title>
   </head>
   <body>
