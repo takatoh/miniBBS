13d5f0979b760e8b5b096c4fd4297c351ba62ea7
takatoh <takatoh.m@gmail.com>
2016-01-17 14:14:45 +0900

    comment.php: Provide necessary ';'.

--- comment.php
+++ comment.php
@@ -10,24 +10,24 @@
 <html>
   <head>
     <meta charset="UTF-8" />
-    <title><?php echo $bbsTitle ?></title>
+    <title><?php echo $bbsTitle; ?></title>
     <link rel="stylesheet" type="text/css" href="css/style.css" />
   </head>
   <body>
     <div id="container">
       <div id="header">
-        <h1><a href="index.php"><?php echo $bbsTitle ?></a></h1>
+        <h1><a href="index.php"><?php echo $bbsTitle; ?></a></h1>
       </div>
 
       <div id="main">
         <div class="message">
-          <span>[<?php echo $message->id ?>]</span> <span><?php echo $message->subject ?></span><br />
-          <span><?php echo $message->name ?></span><br />
+          <span>[<?php echo $message->id; ?>]</span> <span><?php echo $message->subject; ?></span><br />
+          <span><?php echo $message->name; ?></span><br />
           <p>
-            <?php echo h($message->content) ?>
+            <?php echo h($message->content); ?>
           </p>
           <div class="message_footer">
-            <?php echo $message->timestamp ?>
+            <?php echo $message->timestamp; ?>
           </div>
           <?php foreach ($message->comments as $comment) { ?>
             <div class="comment">
@@ -42,7 +42,7 @@
 
         <div id="comment_form">
           <form method="POST" action="index.php">
-            <input type="hidden" name="message_id" value="<?php echo $message->id ?>"/>
+            <input type="hidden" name="message_id" value="<?php echo $message->id; ?>"/>
             <table>
               <tr><td class="col1">Name:</td><td class="col2"><input type="text" name="name" value="no name" /></td></tr>
               <tr><td class="col1">Comment:</td><td class="col2"><textarea name="content" rows=8 cols=50 /></textarea></td></tr>
